207cc44a72e8a32d919688a4f85ce2a28982605a
Frans <97081479+Fransssss@users.noreply.github.com>
2022-08-23 08:14:47 -0700

    Update ValidReviewRange.h

--- ValidReviewRange.h
+++ ValidReviewRange.h
@@ -1,6 +1,5 @@
-//
-// Created by XaveF on 8/22/2022.
-//
+// Created by Franisksus Agapa on 8/22/2022.
+
 
 #ifndef BASICCLASS3_VALIDREVIEWRANGE_H
 #define BASICCLASS3_VALIDREVIEWRANGE_H
@@ -8,7 +7,7 @@
 
 int ValidReview(int& userReview)
 {
-    if(userReview > 10)
+    if(userReview > 10)              // more than what is expected
     {
         userReview == 0;
     }
